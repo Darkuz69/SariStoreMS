c9fc30a49072f18ee26871982db616fd8334f442
Michael John Larido <lakatangaming145@gmail.com>
2025-07-24 21:45:36 +0800

    adjusted permission list

---
+++ server/src/config/initialize.js
@@ -0,0 +1,9 @@
+import Permission from "../models/Permissions.js";
+import Role from "../models/Roles.js";
+import RolePermission from "../models/RolePermissions.js";
+
+import Account from "../models/Accounts.js";
+import AccountRole from "../models/AccountRoles.js";
+
+import sequelize from "./database.js";
+
